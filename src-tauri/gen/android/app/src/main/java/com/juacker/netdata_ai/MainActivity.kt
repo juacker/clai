139a9b9e2be3cbabd7d92b86bca52ebd63bdfb9b
juan <2930882+juacker@users.noreply.github.com>
2025-10-26 17:34:46 +0100

    init

---
+++ src-tauri/gen/android/app/src/main/java/com/juacker/netdata_ai/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.juacker.netdata_ai
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
